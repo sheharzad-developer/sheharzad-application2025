b20a53d5417ee62530b746f01c881c33984cd17f
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2025-08-31 19:51:09 +0500

    Implement ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-3xl text-violent-700 font-black'>Welome to Next.js 👍</h1>
+    </div>
   )
 }
 
